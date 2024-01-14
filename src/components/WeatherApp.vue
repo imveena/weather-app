8f8915dc966bf73f59ecc30b549036b10741a487
veena <veenahn.94@gmail.comm>
2024-01-14 23:38:04 +0530

    Add background colour change on temprature changes

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <div class="main">
-      <div class="search-box">
+    <div class="main" v-bind:class="{warm: data.state_weather}" >
+<div class="search-box">
         <input class="search-bar" type="text" placeholder="search......" v-model="data.city" 
                 @keyup.enter="getApi()" >
-       <div >
+       <div v-if="data.weather">
        <div class="header">
-           <h1> {{data.city}} </h1>
+           <h1> {{data.weather.name}} </h1>
            <h3> {{new Date().toLocaleString()}} </h3>
       </div>
        <div class="temp">
-           <h2>12&deg;</h2>
+           <h2>{{Math.round(data.weather.main.temp)}}&deg;</h2>
       </div>
       <div class="state">
-           <h3> cloud </h3>
+           <h3>{{ data.weather.weather[0].main }} </h3>
       </div>
       </div>
       </div>
@@ -30,17 +30,35 @@ export default {
   data(){
     return{
       data : {
+        weather:null,
         city:'',
-        api_key:'6077fd7b0d301e8bd0fccf10d3b3c748'
+        api_key:'6077fd7b0d301e8bd0fccf10d3b3c748',
+        current_day:'',
+        state_weather:false
     }
     }
   },
+  mounted:async function(){
+    this.getApi()
+  },
+
   methods:{
-    async getApi(){
+     async getApi(){
+      if(this.data.city===''){
+        this.data.city='Hubli'
+      }
       const getWeather = await axios.get( 
         `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${this.data.city}&appid=${this.data.api_key}`
       )
-      console.log(getWeather)
+      this.data.weather = getWeather.data
+      this.data.city=''
+      this.data.state_weather = false
+      if(this.data.weather.main.temp > 20){
+        this.data.state_weather = true
+      }else{
+        this.data.state_weather = false
+      }
+      console.log(this.data.weather)
     }
   }
 }
@@ -52,20 +70,34 @@ export default {
   padding:0;
   box-sizing: border-box;
 }
+
 #app{
   background-image: url('https://img.freepik.com/free-vector/gorgeous-clouds-background-with-blue-sky-design_1017-25501.jpg');
   background-size: cover;
   background-position: bottom;
-  transition: 0.4s;
+  /* transition: 0.4s;  */
 }
 .main{
   min-height: 100vh;
   padding:25px;
-  background-image: linear-gradient(to baottom,rgba(18,75,156,0.35),rgba(2,15,22,0.75));
+  background-image: linear-gradient(
+    to baottom,
+    rgba(18,75,156,0.35),
+    rgba(2,15,22,0.75));
+
+}
+.main.warm{
+  background-image: linear-gradient(
+    to bottom,
+    rgba(212, 69, 69, 0.35),
+    rgba(246, 6, 6, 0.75)
+  );
+  min-height: 100vh;
+  padding:25px;
 }
 .search-box{
   width:100%;
-  left:300px;
+  left:100px;
   margin:200px;
   position:relative;
 }
